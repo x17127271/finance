29a4278f68027d6dad3e1a425ac6307a2bd0bcab
Francisco <x17127271@student.ncirl.ie>
2020-05-16 10:57:49 +0100

    Cases basic CRUD

--- financecustomer/app/controllers/cases_controller.rb
+++ financecustomer/app/controllers/cases_controller.rb
@@ -1,19 +1,39 @@
 class CasesController < ApplicationController
   def index
+    @cases = Case.all
   end
 
   def new
+    @case = Case.new
   end
 
   def create
+    @case = Case.create(params.require(:case).permit(:customer_id, :topic, :comments))
+    if @case.valid?
+      redirect_to customer_path(@case.customer)
+    else
+      flash[:errors] = @case.errors.full_messages
+      redirect_to new_case_path
+    end
   end
 
   def show
+    @case = Case.find(params[:id])
   end
 
   def edit
+    @case = Case.find(params[:id])
   end
 
   def update
+    @case = Case.find(params[:id])
+    @case.update(params.require(:case).permit(:topic, :comments))
+    redirect_to case_path(@case)
+  end
+
+  def destroy
+    @case = Case.find(params[@id])    
+    @case.destroy
+    redirect_to cases_path
   end
 end
